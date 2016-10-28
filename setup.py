f07a2609bf513a7540048b568aa80aab9043bf26
Alex Claman <claman@alexclaman.com>
2016-10-27 21:09:56 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+  readme = f.read()
+
+with open('LICENSE') as f:
+  license = f.read()
+
+setup(
+  name='percival',
+  version='0.9.0',
+  description='Search and edit your Markdown reading list',
+  long_description=readme,
+  author='Alex Claman',
+  author_email='claman@alexclaman.com',
+  url='https://github.com/claman/percival',
+  license='license'
+)
